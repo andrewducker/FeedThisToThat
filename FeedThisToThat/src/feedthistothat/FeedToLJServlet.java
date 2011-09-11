9e5b9dda5341332b4713b0cfa7f4ee2d06dd053e
Andrew Ducker <andrew@ducker.org.uk>
2011-09-11 21:06:33 +0100

    Upgrading to 1.5.2 and moving the Delicious-reading code into its own class.

--- FeedThisToThat/src/feedthistothat/FeedToLJServlet.java
+++ FeedThisToThat/src/feedthistothat/FeedToLJServlet.java
@@ -2,77 +2,24 @@ package feedthistothat;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Vector;
-
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import uk.org.catnip.eddie.FeedData;
-import uk.org.catnip.eddie.parser.Parser;
-import uk.org.catnip.eddie.Category;
-import uk.org.catnip.eddie.Detail;
-import uk.org.catnip.eddie.Entry;
-import uk.org.catnip.eddie.Link;;
+import javax.servlet.http.HttpServletResponse;;
 
 
 @SuppressWarnings("serial")
 public class FeedToLJServlet extends HttpServlet {
-	@SuppressWarnings("unchecked")
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 
-		resp.setContentType("text/HTML");
 
 		String userName="andrewducker";
-		
-		String feedURL = "http://feeds.delicious.com/v2/rss/"+userName+"?count=30";
-		
-		byte[] feedContents = FeedReader.Read(feedURL);
-		
-		Parser parser = new Parser();
-		FeedData feedData = null;
-		feedData = parser.parse(feedContents);
-		PrintWriter writer = resp.getWriter();
-		writer.println("<ul class=\"delicious\">");
-		
-		for(Iterator<Entry> entryIterator = feedData.entries();entryIterator.hasNext();)
-		{
-			Entry entry = entryIterator.next();
-			String title = entry.getTitle().getValue();
-			String link = "";
-			String comment = "";
-			List<String> tags = new Vector<String>();
-			Detail commentDetail = entry.getSummary();
-			if(commentDetail != null)
-			{
-				 comment = commentDetail.getValue();
-			}
-			for(Iterator<Link> linkIterator = entry.links();linkIterator.hasNext();)
-			{
-				 link =linkIterator.next().getValue();
-			}
-			for(Iterator<Category> linkIterator = entry.categories();linkIterator.hasNext();)
-			{
-				 tags.add(linkIterator.next().getTerm());
-			}
+		String output = ReadFromDelicious.Read(userName);
 
-			writer.println("<li><div class=\"delicious-link\"><A href=\""+link+"\">" +title+"</A></div>");
-			if(comment != "")
-			{
-				writer.println("<div class=\"delicious-extended\">"+comment+"</div>");
-			}
-			writer.println("<div class=\"delicious-tags\">(tags:");
-			for (String tag : tags) {
-				writer.print(" <a href=\"http://delicious.com/andrewducker/"+tag+"\">"+tag+"</a>");
-			}
-			writer.print(")</div>");
-			
-			writer.println("</li>");
-		}
-		writer.println("</ul>");
+
+		resp.setContentType("text/HTML");
+		PrintWriter writer = resp.getWriter();
+		writer.println(output);
 		
 //	    WriteToLJ writer = new WriteToLJ();
 //	    writer.Write();
---
+++ FeedThisToThat/src/feedthistothat/ReadFromDelicious.java
@@ -0,0 +1,67 @@
+package feedthistothat;
+
+import java.io.IOException;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
+
+import uk.org.catnip.eddie.Category;
+import uk.org.catnip.eddie.Detail;
+import uk.org.catnip.eddie.Entry;
+import uk.org.catnip.eddie.FeedData;
+import uk.org.catnip.eddie.Link;
+import uk.org.catnip.eddie.parser.Parser;
+
+public class ReadFromDelicious {
+	@SuppressWarnings("unchecked")
+	public static String Read(String userName) throws IOException
+	{
+		String feedURL = "http://feeds.delicious.com/v2/rss/"+userName+"?count=30";
+		
+		byte[] feedContents = FeedReader.Read(feedURL);
+		
+		Parser parser = new Parser();
+		FeedData feedData = null;
+		feedData = parser.parse(feedContents);
+		
+		String output = "<ul class=\"delicious\">";
+		
+		for(Iterator<Entry> entryIterator = feedData.entries();entryIterator.hasNext();)
+		{
+			Entry entry = entryIterator.next();
+			String title = entry.getTitle().getValue();
+			String link = "";
+			String comment = "";
+			List<String> tags = new Vector<String>();
+			Detail commentDetail = entry.getSummary();
+			if(commentDetail != null)
+			{
+				 comment = commentDetail.getValue();
+			}
+			for(Iterator<Link> linkIterator = entry.links();linkIterator.hasNext();)
+			{
+				 link =linkIterator.next().getValue();
+			}
+			for(Iterator<Category> linkIterator = entry.categories();linkIterator.hasNext();)
+			{
+				 tags.add(linkIterator.next().getTerm());
+			}
+
+			output += "<li><div class=\"delicious-link\"><A href=\""+link+"\">" +title+"</A></div>";
+			if(comment != "")
+			{
+				output += "<div class=\"delicious-extended\">"+comment+"</div>";
+			}
+			output += "<div class=\"delicious-tags\">(tags:";
+			for (String tag : tags) {
+				output +=" <a href=\"http://delicious.com/andrewducker/"+tag+"\">"+tag+"</a>";
+			}
+			output+= ")</div>";
+			
+			output += "</li>";
+		}
+		output += "</ul>";
+		return output;
+	}
+}
