2cca56f3c9ba63419ff18deb5f2bab2edeecfe1a
Andrew Ducker <andrew@ducker.org.uk>
2011-09-11 17:52:18 +0100

    Now putting tags in.

--- FeedThisToThat/src/feedthistothat/FeedToLJServlet.java
+++ FeedThisToThat/src/feedthistothat/FeedToLJServlet.java
@@ -3,6 +3,8 @@ package feedthistothat;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import uk.org.catnip.eddie.FeedData;
 import uk.org.catnip.eddie.parser.Parser;
+import uk.org.catnip.eddie.Category;
 import uk.org.catnip.eddie.Detail;
 import uk.org.catnip.eddie.Entry;
 import uk.org.catnip.eddie.Link;;
@@ -23,7 +26,9 @@ public class FeedToLJServlet extends HttpServlet {
 
 		resp.setContentType("text/HTML");
 
-		String feedURL = "http://feeds.delicious.com/v2/rss/andrewducker?count=30";
+		String userName="andrewducker";
+		
+		String feedURL = "http://feeds.delicious.com/v2/rss/"+userName+"?count=30";
 		
 		byte[] feedContents = FeedReader.Read(feedURL);
 		
@@ -39,6 +44,7 @@ public class FeedToLJServlet extends HttpServlet {
 			String title = entry.getTitle().getValue();
 			String link = "";
 			String comment = "";
+			List<String> tags = new Vector<String>();
 			Detail commentDetail = entry.getSummary();
 			if(commentDetail != null)
 			{
@@ -48,11 +54,21 @@ public class FeedToLJServlet extends HttpServlet {
 			{
 				 link =linkIterator.next().getValue();
 			}
+			for(Iterator<Category> linkIterator = entry.categories();linkIterator.hasNext();)
+			{
+				 tags.add(linkIterator.next().getTerm());
+			}
+
 			writer.println("<li><div class=\"delicious-link\"><A href=\""+link+"\">" +title+"</A></div>");
 			if(comment != "")
 			{
 				writer.println("<div class=\"delicious-extended\">"+comment+"</div>");
 			}
+			writer.println("<div class=\"delicious-tags\">(tags:");
+			for (String tag : tags) {
+				writer.print(" <a href=\"http://delicious.com/andrewducker/"+tag+"\">"+tag+"</a>");
+			}
+			writer.print(")</div>");
 			
 			writer.println("</li>");
 		}
