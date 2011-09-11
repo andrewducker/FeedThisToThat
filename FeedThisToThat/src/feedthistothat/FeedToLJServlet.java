261fe063ca6e589fb2de5b7ede854131246d0309
Andrew Ducker <andrew@ducker.org.uk>
2011-09-11 21:39:09 +0100

    Now logs in to LJ using the challenge authorisation, rather than plain text.

--- FeedThisToThat/src/feedthistothat/FeedToLJServlet.java
+++ FeedThisToThat/src/feedthistothat/FeedToLJServlet.java
@@ -1,10 +1,9 @@
 package feedthistothat;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;;
+import javax.servlet.http.HttpServletResponse;
 
 
 @SuppressWarnings("serial")
@@ -13,17 +12,20 @@ public class FeedToLJServlet extends HttpServlet {
 			throws IOException {
 
 
-		String userName="andrewducker";
-		String output = ReadFromDelicious.Read(userName);
-
-
-		resp.setContentType("text/HTML");
-		PrintWriter writer = resp.getWriter();
-		writer.println(output);
+//		String userName="andrewducker";
+//		String output = ReadFromDelicious.Read(userName);
+//
+//
+//		resp.setContentType("text/HTML");
+//		PrintWriter writer = resp.getWriter();
+//		writer.println(output);
 		
-//	    WriteToLJ writer = new WriteToLJ();
-//	    writer.Write();
-        
-		//resp.getWriter().println(feedContents);
+	    WriteToLJ writer = new WriteToLJ("andrewducker","Balisk!");
+		try {
+			resp.getWriter().println(writer.Write());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- FeedThisToThat/src/feedthistothat/WriteToLJ.java
+++ FeedThisToThat/src/feedthistothat/WriteToLJ.java
@@ -1,29 +1,60 @@
 package feedthistothat;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 
 public class WriteToLJ {
 
-	public void Write(){
+	private String userName;
+	private String password;
+	public WriteToLJ(String UserName, String Password){
+		userName = UserName;
+		password = Password;
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public String Write() throws MalformedURLException, XmlRpcException, NoSuchAlgorithmException{
 	    XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
-	    try {
-			config.setServerURL(new URL("http://www.livejournal.com/interface/xmlrpc"));
+		config.setServerURL(new URL("http://www.livejournal.com/interface/xmlrpc"));
 	    XmlRpcClient client = new XmlRpcClient();
 	    client.setConfig(config);
-	    HashMap login = new HashMap();
-	    login.put("username", "AndrewDucker");
-	    login.put("password", "Balisk!");
+	    
+		Map<String, Object> result =  (Map<String, Object>) client.execute("LJ.XMLRPC.getchallenge", new Object[0]);
+	    
+	    String challenge = (String) result.get("challenge");
+	    
+	    String response = MD5Hex(challenge+MD5Hex(password));
+	    
+	    HashMap<String,String> login = new HashMap<String,String>();
+	    login.put("username", userName);
+	    login.put("auth_method", "challenge");
+	    login.put("auth_challenge", challenge);
+	    login.put("auth_response", response);
 	    
 	    Object[] params = new Object[]{login};
-	    Map result =  (Map) client.execute("LJ.XMLRPC.login", params);
-	    System.out.println(result.get("fullname"));
-	    } catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		result =  (Map<String, Object>) client.execute("LJ.XMLRPC.login", params);
+	    return (String)result.get("fullname");
 	}
+	
+	private static String MD5Hex(String s) throws NoSuchAlgorithmException{
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        byte[] digest = md5.digest(s.getBytes());
+        return toHex(digest);
+	}
+    public static String toHex(byte[] a) {
+        StringBuilder sb = new StringBuilder(a.length * 2);
+        for (int i = 0; i < a.length; i++) {
+            sb.append(Character.forDigit((a[i] & 0xf0) >> 4, 16));
+            sb.append(Character.forDigit(a[i] & 0x0f, 16));
+        }
+        return sb.toString();
+    }
 }
