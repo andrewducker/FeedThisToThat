ac1c8c7d6d190075284f6591bba07ecb6513f0d8
Andrew Ducker <andrew@ducker.org.uk>
2011-09-29 16:40:35 +0100

    Now using the WriterFactory correctly.

--- FeedThisToThat/src/feedthistothat/FeedToLJServlet.java
+++ FeedThisToThat/src/feedthistothat/FeedToLJServlet.java
@@ -15,69 +15,44 @@ public class FeedToLJServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 
-		String deliciousUserName = "andrewducker";
-		String livejournalUserName = "andrewducker";
-		String livejournalPassword = "Balisk!";
-		String timeZoneID = "Europe/London";
-		int timeOfDay = 12;
-		Boolean testMode = true;
-		TimeZone timeZone = TimeZone.getTimeZone(timeZoneID);
-
-		feedSourceToLJ(resp, deliciousUserName, livejournalUserName,
-				livejournalPassword, testMode,timeOfDay, timeZone);
 	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 
 		String sourceUserName = req.getParameter("SourceUserName");
-		String livejournalUserName = req.getParameter("LJUserName");
-		String livejournalPassword = req.getParameter("LJPassword");
-		Boolean testMode = req.getParameter("TestMode") != null;
-		int timeOfDay = Integer.parseInt(req.getParameter("TimeOfDay"));
 		
 		String timeZoneID = "Europe/London";
 		TimeZone timeZone = TimeZone.getTimeZone(timeZoneID);
 
 		
-		feedSourceToLJ(resp, sourceUserName, livejournalUserName,
-				livejournalPassword, testMode, timeOfDay, timeZone);
-	}
-
-	private void feedSourceToLJ(HttpServletResponse resp,
-			String deliciousUserName, String livejournalUserName,
-			String livejournalPassword, Boolean testMode, int timeOfDay, TimeZone timeZone) {
-		ILinkSourceReader reader = new DeliciousReader(deliciousUserName);
+		ILinkSourceReader reader = new DeliciousReader(sourceUserName);
+		
 		
 		List<LinkEntry> links;
 		try {
 			links = reader.Read();
-
+		
 			Calendar endTime = Calendar.getInstance(timeZone);
-
+		
+			int timeOfDay = Integer.parseInt(req.getParameter("TimeOfDay"));
 			endTime.set(Calendar.HOUR_OF_DAY, timeOfDay);
 			endTime.set(Calendar.MINUTE, 0);
 			
 			links = FilterLinksByDate(links, endTime);
-
+		
 			String output = LinkPostFormatter.Format(links);
-
+		
 			resp.setContentType("text/HTML");
-
-			IWriter writer = null;
-			if (testMode) {
-				writer = new ResponseWriter(resp.getWriter());
-			} else {
-				writer = new DWWriter(livejournalUserName, livejournalPassword,
-						timeZone);
-			}
-
+		
+			IWriter writer = WriterFactory.GetWriter(req, resp, timeZone);
 			resp.getWriter().println(writer.Write(output));
-
+		
 		} catch (Exception e1) {
 			e1.printStackTrace();
 		}
 	}
+
 
 	private List<LinkEntry> FilterLinksByDate(List<LinkEntry> links, Calendar endTime) {
 		Calendar startTime = (Calendar) endTime.clone();
--- FeedThisToThat/src/feedthistothat/WriterFactory.java
+++ FeedThisToThat/src/feedthistothat/WriterFactory.java
@@ -1,18 +1,33 @@
 package feedthistothat;
 
-import java.util.Vector;
+import java.util.TimeZone;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class WriterFactory {
-	public WriterFactory() {
-		writers = new Vector<String>();
-		writers.add("Dreamwidth");
-		writers.add("Livejournal");
-		writers.add("Test");
+	public enum Writer{
+		Dreamwidth,
+		Livejournal,
+		Test
 	}
 
-	private Vector<String> writers;
+	public Writer[] getWriters(){
+		return Writer.values();
+	}
 	
-	public Vector<String>getWriters(){
-		return writers;
+	public static IWriter GetWriter(HttpServletRequest req, HttpServletResponse resp, TimeZone timeZone) throws Exception{
+		String destinationUserName = req.getParameter("DestinationUserName");
+		String destinationPassword = req.getParameter("DestinationPassword");
+		String destination = req.getParameter("OutputTo");
+		switch (Writer.valueOf(destination)) {
+		case Dreamwidth:
+			return new DWWriter(destinationUserName, destinationPassword, timeZone);
+		case Livejournal:
+			return new LJWriter(destinationUserName, destinationPassword, timeZone);
+		case Test:
+			return new ResponseWriter(resp.getWriter());
+		default:
+			throw new Exception("No such destination - " + destination);
+		}
 	}
 }
